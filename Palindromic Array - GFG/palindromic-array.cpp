fd6673c5db7e00fe95793fea978b3719ded5fde6
Garima Chandra <garimachandra2002@gmail.com>
2023-04-23 14:14:04 +0530

    Added solution - LeetHub

--- Palindromic Array - GFG/palindromic-array.cpp
+++ Palindromic Array - GFG/palindromic-array.cpp
@@ -13,19 +13,25 @@ public:
     {
     	// code here
     	int i,reverse =0 ,b, remainder1;
+    	//traverse all the elements
     	for(i=0;i<n;i++)
        {   
+           //store each element in a variable to later match it with its reversed value
            b=a[i];
+           //reverse the no.
            while(b!=0)
            {
             remainder1= b%10;
             reverse = reverse*10+remainder1;
             b/=10;
            }
+           //if any element doesnt match with its reverse , return 0 (not a palindrome)
            if(reverse != a[i])
            {
                return 0;
            }
+           
+           //reinitialize reverse as 0 for the next element 
            reverse =0;
        }
        return 1;
