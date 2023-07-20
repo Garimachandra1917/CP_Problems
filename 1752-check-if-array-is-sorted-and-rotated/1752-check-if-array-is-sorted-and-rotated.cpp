98841e0072a9993927d9978c465b88daf43e0a5d
Garima Chandra <garimachandra2002@gmail.com>
2023-07-20 15:28:59 +0530

    Time: 6 ms (24.50%), Space: 8.3 MB (97.37%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,31 +1,140 @@
+// //T(N^2)
+// //S(1)
+
+// class Solution {
+// public:
+//     bool check(vector<int>& nums) {
+//     int n= nums.size(),c=1;
+        
+//         //itterate through the array ckecking by making each element as the start
+//         for(int i=0;i<n;i++)
+//         {  
+//             int c=1;
+//             // now checking from the ith location to remaining n-1 elements
+//             //are they in sorted order
+//             for(int j=0;j<n-1;j++)
+//             {   
+//                 //using modulo n to move towards the back of the array as well
+//                 if(nums[(i+j)%n]<=nums[(i+j+1)%n])
+//                 { 
+//                     c++;
+//                 }
+//             }
+//             // when c==n i.e each element after the start is sorted 
+//               if(c==n)
+//               {
+//                     return true;
+//               }
+//         }
+        
+//         //if c never became n i.e its not a rotated sorted array
+//     return false ;
+//     }    
+        
+// };
+
+//T(N)
+//S(1)
+
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        int n= nums.size(),c=1;
         
-        //itterate through the array ckecking by making each element as the start
+        int n=nums.size(),c=0;
         for(int i=0;i<n;i++)
-        {  
-            int c=1;
-            // now checking from the ith location to remaining n-1 elements
-            //are they in sorted order
-            for(int j=0;j<n-1;j++)
-            {   
-                //using modulo n to move towards the back of the array as well
-                if(nums[(i+j)%n]<=nums[(i+j+1)%n])
-                { 
-                    c++;
-                }
+        { 
+            if(nums[i%n]>nums[(i+1)%n])
+            {
+                c++;
             }
-            // when c==n i.e each element after the start is sorted 
-              if(c==n)
-              {
-                    return true;
-              }
         }
-        
-        //if c never became n i.e its not a rotated sorted array
-    return false ;
+        if(c<=1)
+            return true;
+        else
+             return false;
     }
-        
 };
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
