daf272c84b744739451f6b9a4a1d342d77ef7f98
Garima Chandra <garimachandra2002@gmail.com>
2023-07-21 22:42:38 +0530

    Time: 15 ms (94.86%), Space: 18 MB (39.68%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,17 +1,54 @@
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+//       sort(nums.begin(),nums.end());
+//         int n=nums.size(),i;
+//         for(i=0;i<n;i++)
+//         {
+//             if(i!=nums[i])
+//                 return i;
+//         }
+//         if(i==n)
+//         {
+//             return n;
+//         }
+//         return -1;
+//     }
+// };
+
+//--------------------------------------------------------------------------------------
+
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-      sort(nums.begin(),nums.end());
-        int n=nums.size(),i;
-        for(i=0;i<n;i++)
+        int n=nums.size();
+        int sum1=(n*(n+1))/2;
+        int sum2=0;
+        for(int i=0;i<n;i++)
         {
-            if(i!=nums[i])
-                return i;
+            sum2+=nums[i];
         }
-        if(i==n)
-        {
-            return n;
-        }
-        return -1;
+
+            return sum1-sum2;
+        
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
