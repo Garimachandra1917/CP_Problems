866b972d6f9560b73b0847e99fd4906eb48484cb
Garima Chandra <garimachandra2002@gmail.com>
2023-07-21 12:33:26 +0530

    Time: 33 ms (70.71%), Space: 25.6 MB (14.75%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,67 @@
+//1.T(N^2) , S(1)
+//-----------------------------------------------
+
+// class Solution {
+// public:
+//     void rotate(vector<int>& nums, int k) {
+//         int n=nums.size();
+//         while(k--)
+//         {
+//             int last= nums[n-1];
+//             for(int i=n-2;i>=0;i--)
+//             {
+//                 nums[i+1]=nums[i];
+//             }
+//             nums[0]=last;
+//         }
+        
+//     }
+// };
+//-------------------------------------------------------
+
+//2.T(N) ,S(d)
+//---------------------------------------------------------------
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n= nums.size();
+        //step.1 : effective rotations
+        int d=k%n;
+        
+        //step.2 : moving the d elements in an auxiliary array/vector of d size (S(d))
+        vector<int> v;
+          int j=n-1,p=d;
+        while(p--)
+        {
+          v.push_back(nums[j]);
+            j--;
+        }
+        
+        //step.3 : shift the elements backward
+        for(int i=n-d-1;i>=0;i--)
+        {
+            nums[d+i]=nums[i];
+        }
+        
+        //step.4 : put back the elements from the auxiliary array back to the main array 
+        for(int i=d-1;i>=0;i--)
+        {
+            nums[abs(i-(d-1))]=v[i];
+        }
+        
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
