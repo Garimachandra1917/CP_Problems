ff2f399736fd4af4ecab966da2ec6064335d1ad8
Garima Chandra <garimachandra2002@gmail.com>
2023-07-29 08:34:40 +0530

    Time: 124 ms (77.27%), Space: 66.9 MB (53.99%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -3,33 +3,103 @@
 
 
 //2. Better T(NlongN) S(1)
+// class Solution {
+// public:
+//     int longestConsecutive(vector<int>& nums) {
+//        int n=nums.size();
+//         int lastSmaller =INT_MIN;//to check if the previous element exits
+//         int cnt=0,longSec=0;
+        
+//         //sort the array to club elements
+//         sort(nums.begin(),nums.end());
+        
+//         for(int i=0;i<n;i++)//while itterating through each element
+//         {
+//             if(nums[i]-1==lastSmaller)//if prev element exits put in seq
+//             {
+//                 cnt++;
+//                 lastSmaller =nums[i];
+//             }
+//             //if we encounter the same element again we dont do anything just inc. pointer
+            
+//             else if(nums[i]!=lastSmaller)//when curr element not a part of seq also not equal to the prev                                               one ->start fresh 
+//             {
+//                 cnt=1;
+//                 lastSmaller = nums[i];
+//             }
+//            longSec = max(longSec,cnt); //update the longset seq 
+//         }
+       
+//         return longSec;
+//     }
+// };
+
+
+//3. Using Unordered set 
+//S(N) best->T(3N) worst(N^2)
+
+//2. Better T(NlongN) S(1)
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
        int n=nums.size();
-        int lastSmaller =INT_MIN;//to check if the previous element exits
         int cnt=0,longSec=0;
-        
-        //sort the array to club elements
-        sort(nums.begin(),nums.end());
-        
-        for(int i=0;i<n;i++)//while itterating through each element
+        unordered_set <int> set;
+        for(int i=0;i<n;i++)//insert elements in set {T(1) for each element}
         {
-            if(nums[i]-1==lastSmaller)//if prev element exits put in seq
-            {
-                cnt++;
-                lastSmaller =nums[i];
-            }
-            //if we encounter the same element again we dont do anything just inc. pointer
-            
-            else if(nums[i]!=lastSmaller)//when curr element not a part of seq also not equal to the prev one ->start fresh 
+            set.insert(nums[i]);
+        }
+        
+        for(auto i: set)
+        {
+            if(set.find(i-1)==set.end())//if the i-1 do not exists in set ->i=start else move to next elemt
             {
                 cnt=1;
-                lastSmaller = nums[i];
+                int x=i;
+                while(set.find(x+1)!=set.end())//if start found keep on checking for seq . till found
+                {
+                    x=x+1;
+                    cnt++;
+                }
             }
-           longSec = max(longSec,cnt); //update the longset seq 
+            
+            longSec=max(longSec,cnt);//update longest Seq
         }
+        
        
         return longSec;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
