122b06b3edbc8e6a3ea41928996329febaca32e3
Garima Chandra <garimachandra2002@gmail.com>
2023-07-30 10:19:34 +0530

    Time: 104 ms (59.61%), Space: 50 MB (12.78%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -5,13 +5,13 @@ public:
         int j=0;
         map <int,int>map;
         int n=nums.size();
-        map[0]=1;
+        map[0]=1; //for a case where an individual element is a subarray [0,0,0,3]=subarray=4 to consider 3 as a subarray of k=3
         for(int i=0;i<n;i++)
         {
-            prefixSum+=nums[i];
-            int rem=prefixSum - k;
-            cnt+= map[rem];
-            map[prefixSum]++;
+            prefixSum+=nums[i];// calculate prefixSum 
+            int remove=prefixSum - k; //calulate how much we need remove to make the subarray
+            cnt+= map[remove];//how many times "remove" could be removed to get all possible subarrays  
+            map[prefixSum]++;//add current prefixSum also to the hash map  
         }
     
         return cnt;
